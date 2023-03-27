13db4048d936b3f6a246bd4a6895bdcbb7da337b
Vijay <vijaygohel5454@gmail.com>
2023-03-27 12:23:43 +0530

    add validations in flight details modal

--- src/components/FlightDetailsModal/FlightDetailsModal.tsx
+++ src/components/FlightDetailsModal/FlightDetailsModal.tsx
@@ -10,9 +10,9 @@ export interface IFlight {
   to: string
   departureDate: string
   arrivalDate: string
-  ancillaryServices: string[]
-  specialMeals: string[]
-  shoppingItems: string[]
+  ancillaryServices: []
+  specialMeals: []
+  shoppingItems: []
 }
 
 const FlightDetailsModal = (props: any) => {
@@ -20,30 +20,89 @@ const FlightDetailsModal = (props: any) => {
   const [flight, setFlight] = useState<IFlight>()
 
   useEffect(() => {
-    setFlight(props.flight)
+    const temp = props.flight
+    const ancillaryServicesTemp = temp.ancillaryServices.map((item: string) => {
+      return {
+        value: item,
+        error: false,
+      }
+    })
+    const specialMealsTemp = temp.specialMeals.map((item: string) => {
+      return {
+        value: item,
+        error: false,
+      }
+    })
+    const shoppingItemsTemp = temp.shoppingItems.map((item: string) => {
+      return {
+        value: item,
+        error: false,
+      }
+    })
+
+    temp.ancillaryServices = ancillaryServicesTemp
+    temp.specialMeals = specialMealsTemp
+    temp.shoppingItems = shoppingItemsTemp
+
+    setFlight(temp)
   }, [])
 
   const handleClose = () => {
     closeModal()
   }
 
-  const handleChange = (e: any) => {}
+  const handleChange = (
+    e: any,
+    inputField: 'ancillaryServices' | 'specialMeals' | 'shoppingItems',
+    index: number
+  ) => {
+    const temp: any[] = [...flight[inputField]]
+    temp[index].value = e.target.value
+    temp[index].error = false
+    setFlight({ ...flight, [inputField]: temp })
+  }
 
   const handleSave = (e: any) => {
     e.preventDefault()
     e.stopPropagation()
+
+    const temp = JSON.parse(JSON.stringify(flight))
+
+    let hasError: boolean = false
+    ;['ancillaryServices', 'specialMeals', 'shoppingItems'].map(
+      (category: any) => {
+        temp[category].map((item: any, index: number) => {
+          if (
+            !temp[category][index].value ||
+            temp[category][index].value == ''
+          ) {
+            hasError = true
+            temp[category][index].error = true
+          }
+        })
+      }
+    )
+    if (!hasError) {
+      // dispatch(updatePassenger(passenger?.id, passenger) as any)
+      closeModal()
+    } else setFlight(temp)
   }
 
-  const addInput = (inputField: "ancillaryServices" | "specialMeals" | "shoppingItems") => {
-    const temp: string[] = [...flight[inputField]]
-    temp.push("")
-    setFlight({...flight, [inputField]: temp})
+  const addInput = (
+    inputField: 'ancillaryServices' | 'specialMeals' | 'shoppingItems'
+  ) => {
+    const temp: any[] = [...flight[inputField]]
+    temp.push({ value: '', error: false })
+    setFlight({ ...flight, [inputField]: temp })
   }
 
-  const removeInput = (inputField: "ancillaryServices" | "specialMeals" | "shoppingItems", index: number) => {
+  const removeInput = (
+    inputField: 'ancillaryServices' | 'specialMeals' | 'shoppingItems',
+    index: number
+  ) => {
     const temp: string[] = [...flight[inputField]]
     temp.splice(index, 1)
-    setFlight({...flight, [inputField]: temp})
+    setFlight({ ...flight, [inputField]: temp })
   }
 
   return (
@@ -90,65 +149,112 @@ const FlightDetailsModal = (props: any) => {
 
           <Form.Group className="mb-3">
             <Form.Label>Ancillary Services</Form.Label>
-            <Button className='btn' size='sm' onClick={()=>addInput("ancillaryServices")}>Add</Button>
-            {flight?.ancillaryServices.map((service: string, index: number) => (
-              <Row className='my-2' key={`ancillaryServices_${service}`}>
-              <Col xs={10}>
-                <Form.Control
-                  value={service}
-                  type="text"
-                  aria-label={service}
-                  required
-                  name={`ancillaryServices_${service}`}
-                  onChange={handleChange}
-                />
-              </Col>
-              <Col xs={1}>
-                <i className={'fa-solid fa-xmark mt-2'} style={{fontSize: 20}} onClick={()=>removeInput("ancillaryServices", index)}></i>
-              </Col>
-            </Row>
+            <Button
+              className="btn"
+              size="sm"
+              onClick={() => addInput('ancillaryServices')}
+            >
+              Add
+            </Button>
+            {flight?.ancillaryServices.map((service: any, index: number) => (
+              <Row className="my-2" key={`ancillaryServices_${index}`}>
+                <Col xs={10}>
+                  <Form.Control
+                    value={service.value}
+                    type="text"
+                    aria-label={service.value}
+                    required
+                    name={`ancillaryServices_${index}`}
+                    onChange={(e) =>
+                      handleChange(e, 'ancillaryServices', index)
+                    }
+                    isInvalid={service.error}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Please enter Ancillary Service.
+                  </Form.Control.Feedback>
+                </Col>
+                <Col xs={1}>
+                  <i
+                    role="button"
+                    className={'fa-solid fa-xmark mt-2'}
+                    style={{ fontSize: 20 }}
+                    onClick={() => removeInput('ancillaryServices', index)}
+                  ></i>
+                </Col>
+              </Row>
             ))}
           </Form.Group>
 
           <Form.Group className="mb-3">
             <Form.Label>Special meals</Form.Label>
-            <Button className='btn' size='sm' onClick={()=>addInput("specialMeals")}>Add</Button>
-            {flight?.specialMeals.map((meal: string, index: number) => (
-              <Row className='my-2' key={`specialMeal_${meal}`}>
-              <Col xs={10}>
-                <Form.Control
-                  value={meal}
-                  type="text"
-                  aria-label={meal}
-                  required
-                  name={`specialMeal_${meal}`}
-                  onChange={handleChange}
-                />
-              </Col>
-              <Col xs={1}>
-                <i className={'fa-solid fa-xmark mt-2'} style={{fontSize: 20}} onClick={()=>removeInput("specialMeals", index)}></i>
-              </Col>
-            </Row>
+            <Button
+              className="btn"
+              size="sm"
+              onClick={() => addInput('specialMeals')}
+            >
+              Add
+            </Button>
+            {flight?.specialMeals.map((meal: any, index: number) => (
+              <Row className="my-2" key={`specialMeal_${index}`}>
+                <Col xs={10}>
+                  <Form.Control
+                    value={meal.value}
+                    type="text"
+                    aria-label={meal}
+                    required
+                    name={`specialMeal_${index}`}
+                    onChange={(e) => handleChange(e, 'specialMeals', index)}
+                    isInvalid={meal.error}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Please enter Meal.
+                  </Form.Control.Feedback>
+                </Col>
+                <Col xs={1}>
+                  <i
+                    role="button"
+                    className={'fa-solid fa-xmark mt-2'}
+                    style={{ fontSize: 20 }}
+                    onClick={() => removeInput('specialMeals', index)}
+                  ></i>
+                </Col>
+              </Row>
             ))}
           </Form.Group>
 
           <Form.Group className="mb-3">
             <Form.Label>Shopping Items</Form.Label>
-            <Button className='btn' size='sm' onClick={()=>addInput("shoppingItems")}>Add</Button>
-            {flight?.shoppingItems.map((item: string, index: number) => (
-              <Row className='my-2' key={`shoppingItems_${item}`}>
+            <Button
+              className="btn"
+              size="sm"
+              onClick={() => addInput('shoppingItems')}
+            >
+              Add
+            </Button>
+            {flight?.shoppingItems.map((item: any, index: number) => (
+              <Row className="my-2" key={`shoppingItems_${index}`}>
                 <Col xs={10}>
                   <Form.Control
-                    value={item}
+                    value={item.value}
                     type="text"
                     aria-label={item}
                     required
-                    name={`shoppingItems_${item}`}
-                    onChange={handleChange}
+                    name={`shoppingItems_${index}`}
+                    onChange={(e) => handleChange(e, 'shoppingItems', index)}
+                    isInvalid={item.error}
                   />
+                  <Form.Control.Feedback type="invalid">
+                    Please enter Shopping Item.
+                  </Form.Control.Feedback>
                 </Col>
                 <Col xs={1}>
-                  <i className={'fa-solid fa-xmark mt-2'} style={{fontSize: 20}} onClick={()=>removeInput("shoppingItems", index)}></i>
+                  <i
+                    role="button"
+                    className={'fa-solid fa-xmark mt-2'}
+                    style={{ fontSize: 20 }}
+                    onClick={() => removeInput('shoppingItems', index)}
+                  ></i>
                 </Col>
               </Row>
             ))}
